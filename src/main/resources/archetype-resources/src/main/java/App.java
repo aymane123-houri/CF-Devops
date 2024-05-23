100125d574a52dc42926b4fcbfa626485213af32
aymane <houri.aymane03@gmail.com>
2024-05-23 14:49:05 +0100

    ajoute de message par dev

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -5,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Bonjour et bon courage dans votre examen en Devops" );
+        System.out.println( "dev qui fais ce changement" );
     }
 }
